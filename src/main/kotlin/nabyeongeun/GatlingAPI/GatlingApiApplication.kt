d127c9768b03f48aff60dab843ee576c66ce09fa
Na <resurrection04@gmail.com>
2023-11-03 16:48:19 +0900

    first commit

---
+++ src/main/kotlin/nabyeongeun/GatlingAPI/GatlingApiApplication.kt
@@ -0,0 +1,11 @@
+package nabyeongeun.GatlingAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatlingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatlingApiApplication>(*args)
+}
