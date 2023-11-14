cc409156d9c09097ac448f43d07493bfd8ba35c3
nabyeongeun <na.byeongeun@gmail.com>
2023-11-14 23:54:22 +0900

    simple service done

---
+++ README.md
@@ -0,0 +1,15 @@
+# GatlingAPI
+
+RequestList.txt에 발송할 API를 기재하여 실행시키면 순차적으로 API를 발송합니다.  
+GET 또는 POST만 지원되며 POST로 발송하여도 request body가 아닌 URL에 모든 key, value가 입력됩니다.
+
+이 코드는 어플리케이션 테스트 용도로만 사용할 수 있습니다.  
+이 코드를 사용함으로 발생하는 모든 민형사상 책임은 사용자 본인에게 있습니다.
+
+----
+
+write your APIs to be sent in RequestList.txt and run it, the API will be sent sequentially.  
+Only GET or POST are supported, and even if sent via POST, all keys and values are entered in the URL, not the request body.
+
+This code only be used for application testing purposes.  
+All civil and criminal liability arising from the use of this code lies with the user.
--- src/main/kotlin/nabyeongeun/GatlingAPI/GatlingApiApplication.kt
+++ src/main/kotlin/nabyeongeun/GatlingAPI/GatlingApiApplication.kt
@@ -1,11 +1,92 @@
-package nabyeongeun.GatlingAPI
+package nabyeongeun.gatlingAPI
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.ClassPathResource
+import org.springframework.util.FileCopyUtils
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URI
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublisher
+import java.net.http.HttpRequest.BodyPublishers
+import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 @SpringBootApplication
 class GatlingApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatlingApiApplication>(*args)
+//	runApplication<GatlingApiApplication>(*args)
+
+	val lines = mutableListOf<String>()
+
+	val resource = ClassPathResource("RequestList.txt")
+	val inputStream = resource.inputStream
+	val reader = BufferedReader(InputStreamReader(inputStream))
+
+	reader.useLines { lines.addAll(it) }
+
+	if(lines.isEmpty()) {
+		print("RequestList is empty");
+		return;
+	}
+
+	val client = HttpClient.newBuilder().build()
+	val requestBuilder = HttpRequest.newBuilder()
+
+	var presentLine = 1
+	val totalLine = lines.size
+	for(line in lines) {
+
+		val fullURI = line.split(" ")
+
+		val method = fullURI[0]
+		val uri    = fullURI[1]
+
+		if(!method.contains("GET") && !method.contains("POST")) {
+			println("Unsupported http method : $line")
+			break
+		}
+
+		val response = client.send(parameterParser(method, requestBuilder, uri), HttpResponse.BodyHandlers.ofString())
+		println(presentLine.toString() + " of " + totalLine.toString() + " "+ response.uri())
+		print(response.body())
+
+		Thread.sleep(1000) // set delay
+
+		presentLine += 1
+	}
+}
+
+fun parameterParser(method : String, request : HttpRequest.Builder, uri: String) : HttpRequest {
+
+	val paramMap = mutableMapOf<String,String>()
+
+	val queryString = uri.split("?")
+	val prefix = queryString[0]
+	var postfix = "?"
+
+	if(queryString.size > 1) {
+
+		val params = queryString[1].split("&")
+
+		for (param in params) {
+			val keyValue = param.split("=")
+
+			val key = keyValue[0]
+			val value = URLEncoder.encode(keyValue[1], StandardCharsets.UTF_8)
+
+			postfix += "$key=$value&"
+		}
+	}
+
+	return request
+		.uri(URI.create(prefix + postfix))
+		.method(method, BodyPublishers.ofString(""))
+		.header("Content-Type", "application/x-www-form-urlencoded")
+		.build()
 }
